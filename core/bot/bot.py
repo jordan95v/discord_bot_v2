20777a93f64f124ff61dde6182efbf0cc21f7ec2
jordan95v <jordan.dfrsne@gmail.com>
2023-01-13 09:11:24 +0100

    Added docstring. (2)

--- core/bot/bot.py
+++ core/bot/bot.py
@@ -4,9 +4,20 @@ from discord.ext import commands
 
 class DiscordBot(commands.Bot):
     async def on_ready(self):
+        """Print when the bot is ready to be used."""
+
         print(f"Logged on as {self.user}!")
 
     async def create_embed(self, value: str) -> discord.Embed:
+        """Create an embed message.
+
+        Args:
+            value (str): The value of the message.
+
+        Returns:
+            discord.Embed: The embedded message.
+        """
+
         output: discord.Embed = discord.Embed(color=discord.Color.purple())
         output.set_author(name="ChatGPT Response")
         output.add_field(name="", value=value)
