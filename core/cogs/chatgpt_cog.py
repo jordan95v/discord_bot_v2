37c312766124ea67f9a118d6b0c993733a5c5931
jordan95v <jordan.dfrsne@gmail.com>
2023-01-13 09:10:25 +0100

    Added docstring.

--- core/cogs/chatgpt_cog.py
+++ core/cogs/chatgpt_cog.py
@@ -13,6 +13,16 @@ class ChatGPTCog(commands.Cog):
         )
 
     async def _call(self, ctx: commands.Context, prompt: str) -> str:
+        """Call the ChatGPT API.
+
+        Args:
+            ctx (commands.Context): The context.
+            prompt (str): The question to ask.
+
+        Returns:
+            str: The response from the API.
+        """
+
         try:
             res: str = await self.client.completion(prompt=prompt)
         except ChatGPTError:
@@ -22,12 +32,27 @@ class ChatGPTCog(commands.Cog):
 
     @commands.command(name="code")
     async def code(self, ctx: commands.Context, _type: str, *, prompt: str) -> None:
+        """Ask ChatGPT to produce code.
+
+        Args:
+            ctx (commands.Context): The context.
+            _type (str): The langage, used for formatting output.
+            prompt (str): The question.
+        """
+
         res: str = await self._call(ctx, prompt)
         msg: discord.Embed = await self.bot.create_embed(f"```{_type}\n{res}```")
         await ctx.message.reply(embed=msg)
 
     @commands.command(name="ask")
     async def ask(self, ctx: commands.Context, *, prompt: str) -> None:
+        """Ask a question to ChatGPT.
+
+        Args:
+            ctx (commands.Context): The context.
+            prompt (str): The question.
+        """
+
         res: str = await self._call(ctx, prompt)
         msg: discord.Embed = await self.bot.create_embed(f"```{res}```")
         await ctx.message.reply(embed=msg)
